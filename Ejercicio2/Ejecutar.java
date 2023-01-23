a73e2821303c12cddffa60bd169d07594686eaf6
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-01-23 18:19:28 +0100

    Ejercicio2

---
+++ Ejercicio2/Ejecutar.java
@@ -0,0 +1,11 @@
+package Ejercicio2;
+
+    public class Ejecutar {
+
+        public static void main(String[] args) {
+            
+            Perfil Pepe = new Perfil();
+
+            System.out.println("Nombre: " + Pepe.nombre + "\nApellidos: " + Pepe.apellido + "\nEdad: " + Pepe.edad);
+        }
+    }
---
+++ Ejercicio2/Perfil.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+    public class Perfil {
+
+        String nombre = "Pepe";
+        String apellido = "Rodrigez";
+        int edad = 10;
+        }
